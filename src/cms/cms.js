d7553b642e745e9272700aa2364658bf97a5ee44
Pedro Nascimento <nennogabriel@gmail.com>
2019-11-12 13:06:59 -0300

    test 2

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,16 +1,38 @@
-import CMS from 'netlify-cms-app'
-import uploadcare from 'netlify-cms-media-library-uploadcare'
-import cloudinary from 'netlify-cms-media-library-cloudinary'
+import CMS from "netlify-cms-app";
+import uploadcare from "netlify-cms-media-library-uploadcare";
+import cloudinary from "netlify-cms-media-library-cloudinary";
 
-import AboutPagePreview from './preview-templates/AboutPagePreview'
-import BlogPostPreview from './preview-templates/BlogPostPreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
-import IndexPagePreview from './preview-templates/IndexPagePreview'
+import AboutPagePreview from "./preview-templates/AboutPagePreview";
+import BlogPostPreview from "./preview-templates/BlogPostPreview";
+import ProductPagePreview from "./preview-templates/ProductPagePreview";
+import IndexPagePreview from "./preview-templates/IndexPagePreview";
 
-CMS.registerMediaLibrary(uploadcare)
-CMS.registerMediaLibrary(cloudinary)
+// import FileSystemBackend from "netlify-cms-backend-fs";
 
-CMS.registerPreviewTemplate('index', IndexPagePreview)
-CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
-CMS.registerPreviewTemplate('blog', BlogPostPreview)
+// const config = {
+//   display_url: window.location.origin
+// };
+
+// if (process.env.NODE_ENV === "development") {
+//   // config.load_config_file = false
+//   config.backend = {
+//     name: "file-system",
+//     api_root: "/api"
+//   };
+//   CMS.registerBackend("file-system", FileSystemBackend);
+// } else {
+//   config.backend = {
+//     name: "github",
+//     repo: "fikdik/fikdik.com.br",
+//     branch: "master"
+//   };
+// }
+// CMS.init({ config });
+
+CMS.registerMediaLibrary(uploadcare);
+CMS.registerMediaLibrary(cloudinary);
+
+CMS.registerPreviewTemplate("index", IndexPagePreview);
+CMS.registerPreviewTemplate("about", AboutPagePreview);
+CMS.registerPreviewTemplate("products", ProductPagePreview);
+CMS.registerPreviewTemplate("blog", BlogPostPreview);
